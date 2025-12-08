6b6e93ee9622a7b17decd642c3e63473c275f95c
Ahmed Ehab <ahmedmedo012556@gmail.com>
2025-12-08 13:42:07 +0200

    task 1 and task 2 completed

---
+++ videos_from_1to5.php
@@ -0,0 +1,34 @@
+<!-- Task 1 Start-->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset=<?php echo"UTF-8"; ?>>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= "My First PHP Page" ?></title>
+  </head>
+  <body>
+    <div><?php echo "We Love"; ?></div>
+    <div><?php echo "Elzero Channel"; ?></div>
+    <hr>
+  </body>
+</html>
+<!-- Task 1 End-->
+<!-- ----------------------------------------------------------------------------------------------------------------- -->
+<!-- Task 2 Start-->
+ <?php
+    // echo 'Prevent Me From Running Please';
+    /* echo 'Prevent Me From Running Please'; */
+    # echo 'Prevent Me From Running Please';
+
+// not from the task that only for the horizontal line between every task
+echo "<hr>";
+ ?>
+<!-- Task 2 End-->
+<!-- ----------------------------------------------------------------------------------------------------------------- -->
+<!-- Task 3 start-->
+<!-- Task 3 End-->
+<!-- ----------------------------------------------------------------------------------------------------------------- -->
+<!-- ----------------------------------------------------------------------------------------------------------------- -->
+<!-- ----------------------------------------------------------------------------------------------------------------- -->
+
