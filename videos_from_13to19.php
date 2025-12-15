19873ea0eb44a7684ca955ea0f7771520a15188c
Ahmed Ehab <ahmedmedo012556@gmail.com>
2025-12-15 15:21:18 +0200

    task 1 finished

--- videos_from_13to19.php
+++ videos_from_13to19.php
@@ -1,6 +1,26 @@
 <!-- Task 1 Start -->
 <?php
+    $name = "Elzero Courses";
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?php echo $name; ?>">
+    <title>Welcome To <?php echo $name; ?></title>
+  </head>
+  <body>
+    <h1><?php echo $name; ?></h1>
+    <p>Here In <?php echo $name; ?> We Provide Front-End And Back-End Courses</p>
+    <hr>
+    <div><?php echo $name; ?> Is The What You Need.</div>
+    <footer>All Right Reserved To <?php echo $name; ?></footer>
+  </body>
+</html>
 
+<?php
 // Not from the task that only for the horizontal line between every task
 echo "<hr>";
 ?>
